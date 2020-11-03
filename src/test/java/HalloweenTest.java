44698161cf6a390c30904593257d65de547896bf
Javkhaa0128 <javkhaa0128@gmail.com>
2020-11-03 01:49:47 -0500

    no trear or trick commit

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -1,4 +1,10 @@
 public class HalloweenTest {
 
     // I am about to give everyone candy for halloween
+    // are we there yet???
+    // lets get that 120K
+
+    // There will be no treat and trick this year
+    // stay home and be safe and wear mask
+
 }
